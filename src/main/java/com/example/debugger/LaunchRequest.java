2ea088e205f58eb6b9bed8032da7e5dd24bccb29
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-07 23:34:27 -0700

    Create LaunchRequest.java

---
+++ src/main/java/com/example/debugger/LaunchRequest.java
@@ -0,0 +1,19 @@
+package com.example.debugger;
+
+public class LaunchRequest {
+    private String mainClass;
+
+    public LaunchRequest() {
+    }
+
+     public String getMainClass() {
+        return mainClass;
+    }
+
+    public void setMainClass(String mainClass) {
+        this.mainClass = mainClass;
+    }
+
+    }
+
+}
