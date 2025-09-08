5d7c2d8629f84019eaaeda8d918c418e6505a3e1
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-07 19:16:02 -0700

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import JavaDebuggerUI from './components/JavaDebuggerUI';
+
+function App() {
+  return (
+    <div className="App">
+      <JavaDebuggerUI />
+    </div>
+  );
+}
+
+export default App;
