9036ca27bf90d1c2a3b7b5c4255b88d97df58e72
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-07 19:33:08 -0700

    Create DebugCLI.java

---
+++ src/main/java/com/example/debugger/DebugCLI.java
@@ -0,0 +1,10 @@
+package com.example.debugger;
+
+public class DebugCLI {
+    public static void main(String[] args) {
+        System.out.println("Debug CLI placeholder");
+        for (String arg : args) {
+            System.out.println("Arg: " + arg);
+        }
+    }
+}
