5188dcec3f7d5574947756c440a6dee4b87edc75
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-07 19:17:36 -0700

    Create JavaDebuggerUI.js

---
+++ frontend/src/components/JavaDebuggerUI.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function JavaDebuggerUI() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Java Debugger UI</h1>
+      <p>This is a placeholder for the web-based debugger interface.</p>
+    </div>
+  );
+}
