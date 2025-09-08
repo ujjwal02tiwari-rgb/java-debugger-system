3c671ad91d93a52be01f50300ddfef6cd84143d6
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-08 09:41:02 +0530

    Create DebuggerApplication.java

---
+++ src/main/java/com/example/debugger/DebuggerApplication.java
@@ -0,0 +1,11 @@
+package com.example.debugger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebuggerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DebuggerApplication.class, args);
+    }
+}
