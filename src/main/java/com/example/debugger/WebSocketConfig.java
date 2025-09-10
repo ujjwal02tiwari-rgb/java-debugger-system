3879202e966ef3aaa60ad05b2d4bf4273f007632
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-10 06:55:46 +0530

    Update WebSocketConfig.java

--- src/main/java/com/example/debugger/WebSocketConfig.java
+++ src/main/java/com/example/debugger/WebSocketConfig.java
@@ -1,7 +1,7 @@
 package com.example.debugger;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework..simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -9,14 +9,17 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/queue");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*");
+        
     }
 }
