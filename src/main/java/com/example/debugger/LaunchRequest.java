d8c11ac23f819077fe0aed4d6015f3a69fdd7c85
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-10 06:56:28 +0530

    Update LaunchRequest.java

--- src/main/java/com/example/debugger/LaunchRequest.java
+++ src/main/java/com/example/debugger/LaunchRequest.java
@@ -2,18 +2,22 @@ package com.example.debugger;
 
 public class LaunchRequest {
     private String mainClass;
+    private String args;
 
-    public LaunchRequest() {
-    }
+    public LaunchRequest() {}
 
-     public String getMainClass() {
+    public String getMainClass() {
         return mainClass;
     }
-
     public void setMainClass(String mainClass) {
         this.mainClass = mainClass;
     }
 
+    public String getArgs() {
+        return args;
     }
-
+    public void setArgs(String args) {
+        this.args = args;
+    }
 }
+
