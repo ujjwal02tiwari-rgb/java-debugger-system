eed48def73668c7bc36cbd24c45c741d4557404d
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-16 12:37:02 +0530

    Create LaunchRequest.java

---
+++ src/main/java/com/example/debugger/LaunchRequest.java
@@ -0,0 +1,13 @@
+package com.example.debugger.dto;
+
+public class LaunchRequest {
+    private String target;
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+}
