c5302216c1568eeeae66d78ad2c5d82a3af29c8b
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-16 12:55:47 +0530

    Create LaunchRequest.java

---
+++ src/main/java/com/example/debugger/dto/LaunchRequest.java
@@ -0,0 +1,13 @@
+package com.example.debugger.dto;
+
+public class LaunchRequest {
+    private String mainClass;
+
+    public String getMainClass() {
+        return mainClass;
+    }
+
+    public void setMainClass(String mainClass) {
+        this.mainClass = mainClass;
+    }
+}
